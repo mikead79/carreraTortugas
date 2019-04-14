c55ae346650d496f8791db5f8e3b9d893bd8b8b4
Mihai <mikead79@yahoo.com>
2019-04-14 12:08:31 +0200

    La etapa de creare la pantalla de carrera de tortugas con pygame

--- carreraConPygame.py
+++ carreraConPygame.py
@@ -1,37 +1,25 @@
-import pygame
-import sys
+import pygame, sys
 
 class Game():
-    corredores = []
+    runners = []
+    __startLine = 20
+    __finishLine = 620
     
     def __init__(self):
         self.__screen = pygame.display.set_mode((640, 480))
+        self.__background = pygame.image.load("images/background.png")
         pygame.display.set_caption("Carrera de bichos")
-        self.background = pygame.image.load("images/background.png")
-        
-        self.runner = pygame.image.load("images/smallball.png")
     
     def competir(self):
-        x =  0
-        hayGanador = False
-        
-        while not hayGanador:
+        gameOver = False
+        while not gameOver:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
-                    pygame.quit()
-                    sys.exit()
-            self.__screen.blit(self.background, (0, 0))
-            self.__screen.blit(self.runner, (x, 240))
+                    gameOver = True
+            self.__screen.blit(self.__background, (0,0))
             pygame.display.flip()
-            
-            x += 3
-            if x >= 240:
-                hayGanador = True
-        pygame.quit()
-        sys.exit()
+
 if __name__ == "__main__":
-    pygame.init()
     game = Game()
+    pygame.init()
     game.competir()
-    
-        
